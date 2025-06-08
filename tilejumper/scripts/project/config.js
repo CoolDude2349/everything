d96b6b30fca753fbdaa92aab090343690cb4aae6
CoolDude2349 <rer176021@gmail.com>
2025-06-08 16:43:48 -0400

    Add files via upload

---
+++ tilejumper/scripts/project/config.js
@@ -0,0 +1,83 @@
+const config = {
+	BallPrice: 100,
+	JumpScoreRange: [1, 5],
+	
+	GameplayDifficulty: {
+		Levels: {
+			// The higher the value the harder the gameplay
+			Easy: 1,
+			Medium: 1.35,
+			Hard: 2
+		},
+		Endless: {
+			InitSpeed: 1,         // Starting game speed
+			MaxSpeed: 2,          // Maximum game speed
+			SpeedInterval: 1,     // Interval (in seconds) for speed increase
+			SpeedIncrease: 0.002  // How much speed increases per interval
+		}
+	},
+
+	Control: {
+		Keyboard: {
+			Speed: 1200,
+			Accelerate: 5500
+		} 
+	},
+	
+	Tiles: {
+		
+		Horizontal: {
+			Interval: [30, 35],
+			Duration: [1, 1]
+		},
+		
+		Movable: {
+			Interval: [19, 24],
+			Duration: [2, 5]
+		},
+		
+		Trap: {
+			Interval: [26, 29],
+			Duration: [8, 10]
+		},
+	},
+	
+	Collectable: {
+		Interval: [10, 15],
+		Duration: [1, 1]
+	},
+	
+	Colors: [
+		"#478eff",
+		"#8a58fe",
+		"#5ac24c",
+		"#eaa15d",
+		"#ea5d5d"
+	],
+	MusicList: [
+		{MusicName: "Bounce!", FileName: "music4", TilesSpeed: 2200},
+		{MusicName: "Retrowave", FileName: "music0", TilesSpeed: 2000},
+		{MusicName: "Feel My Heart", FileName: "music2", TilesSpeed: 2000},
+		{MusicName: "Love Angel", FileName: "music8", TilesSpeed: 2200},
+		{MusicName: "Happy Birthday", FileName: "music5", TilesSpeed: 2500},
+		{MusicName: "Retro Dance", FileName: "music3", TilesSpeed: 2000},
+		{MusicName: "Move it!", FileName: "music9", TilesSpeed: 2500},
+		{MusicName: "Arabian Night", FileName: "music6", TilesSpeed: 2500},
+		{MusicName: "Happy Friends", FileName: "music7", TilesSpeed: 2500},
+		{MusicName: "Sporty Techno", FileName: "music1", TilesSpeed: 2000},
+		{MusicName: "Rondo Alla Turca", FileName: "music10", TilesSpeed: 2500},
+		{MusicName: "Let's Dance", FileName: "music11", TilesSpeed: 2200},
+		{MusicName: "Crazy Mood", FileName: "music12", TilesSpeed: 2200},
+		{MusicName: "Digitized Soul", FileName: "music13", TilesSpeed: 2000},
+		{MusicName: "ZEST", FileName: "music14", TilesSpeed: 2300},
+		{MusicName: "You and Me", FileName: "music15", TilesSpeed: 2000},
+		{MusicName: "Summer Heart", FileName: "music16", TilesSpeed: 2200},
+		{MusicName: "Vivaldi Spring", FileName: "music17", TilesSpeed: 2200},
+		{MusicName: "Electronic Dance", FileName: "music18", TilesSpeed: 2300},
+		{MusicName: "Echoes of Space", FileName: "music19", TilesSpeed: 2300}
+	]
+}
+
+
+
+export {config} 
