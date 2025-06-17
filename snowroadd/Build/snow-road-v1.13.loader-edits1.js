6fe5e42d6b5b99b519fd2153fa2f6bcfb2320faf
CoolDude2349 <rer176021@gmail.com>
2025-06-17 18:05:23 -0400

    Update and rename snow-road-v1.13.loader-edits.js to snow-road-v1.13.loader-edits1.js

--- snowroadd/Build/snow-road-v1.13.loader-edits1.js
+++ snowroadd/Build/snow-road-v1.13.loader-edits1.js
@@ -384,7 +384,7 @@ function createUnityInstance(t, n, l) {
     }
     ,
     c.fetchWithProgress = function(e, t) {
-        if (e === "https://cdn.jsdelivr.net/gh/cooldude2349/everything@main/snowroadd/StreamingAssets/test_connection.txt") {
+        if (e === "https://1games.io/") {
             console.log("removed garbage")
             e = ""
         }
