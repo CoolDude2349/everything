f073b183a80bde995abe9b5b8a2e88deebdad8b0
CoolDude2349 <rer176021@gmail.com>
2025-07-04 15:09:43 -0400

    Update and rename Build.loader-edits2.js to Build.loader-edits3.js

--- toyrider/Build/Build.loader-edits3.js
+++ toyrider/Build/Build.loader-edits3.js
@@ -1392,7 +1392,7 @@ function createUnityInstance(t, n, d) {
             }));
             return new Promise(function(i, e) {
                 var s = document.createElement("script");
-                s.src = "blob-edit5.js",
+                s.src = "blob-edit6.js",
                 s.onload = function() {
                     if ("undefined" == typeof unityFramework || !unityFramework) {
                         var e, t = [["br", "br"], ["gz", "gzip"]];
