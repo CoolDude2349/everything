d755474a78af4ef964f797e7ee3c126d8f875f7b
CoolDude2349 <rer176021@gmail.com>
2025-07-04 14:12:17 -0400

    Update and rename Build.loader-edit8.js to Build.loader-edit9.js

--- toyrider/Build/Build.loader-edit9.js
+++ toyrider/Build/Build.loader-edit9.js
@@ -1392,7 +1392,7 @@ function createUnityInstance(t, n, d) {
             }));
             return new Promise(function(i, e) {
                 var s = document.createElement("script");
-                s.src = "blob7.js",
+                s.src = "blob8.js",
                 s.onload = function() {
                     if ("undefined" == typeof unityFramework || !unityFramework) {
                         var e, t = [["br", "br"], ["gz", "gzip"]];
